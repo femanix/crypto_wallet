c1dd4a66b24e28e405dfd5c7b2605005bac631a4
Lucas <femani.lucas@hotmail.com>
2022-05-05 16:26:26 -0300

    Refatorando código das seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,32 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Criando moedas padrão..."
+spinner = TTY::Spinner.new("[:spinner] Criando moedas padrão...", format: :spin)
+spinner.auto_spin       
+         
+    coins = [
+        {
+            description: "Bitcoin",
+            acronym: "BTC",
+            url_img: "https://imagensemoldes.com.br/wp-content/uploads/2020/09/Imagem-Dinheiro-Bitcoin-PNG.png"
+        },
 
-Coin.create!(
-    description: "Bitcoin",
-    acronym: "BTC",
-    url_img: "https://imagensemoldes.com.br/wp-content/uploads/2020/09/Imagem-Dinheiro-Bitcoin-PNG.png"
-)
+        {
+            description: "Ethereum",
+            acronym: "ETH",
+            url_img: "https://img1.gratispng.com/20180410/juw/kisspng-ethereum-cryptocurrency-bitcoin-cash-tether-impact-5acceb0643a7c2.9049379315233789502771.jpg"
+        },
 
-puts "Moedas criadas com sucesso!"
+        {
+            description: "Real",
+            acronym: "R$",
+            url_img: "https://img1.gratispng.com/20180627/iqo/kisspng-powell-law-group-p-c-brazilian-real-moeda-de-um-moedas-5b344c79c04c32.3011124415301541057877.jpg"
+        }
+    ]
+        
+    coins.each do |coin|    
+        Coin.find_or_create_by!(coin)
+    end
+
+sleep(2)
+spinner.success("(Moedas criadas com sucesso!)")
