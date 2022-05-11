fdc24e630cc884eec56566d165da9bbe060429bb
Lucas <femani.lucas@hotmail.com>
2022-05-11 16:54:09 -0300

    Removendo o require_tree do aplication cs/js para que os arquivos não sejam carregados por padrão e refatorando o funcionamento dos assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( cable.js coins.js mining_types.js wellcome.js)
+Rails.application.config.assets.precompile += %w( scaffolds.css coins.css mining_types.css wellcome.css)
